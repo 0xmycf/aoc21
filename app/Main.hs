614d32265383d0dd4a51b470d4db4438a9dd5cb4
0xmycf <mycf.mycf.mycf@gmail.com>
2021-12-23 14:26:29 +0100

    Advent of code api, download prompt and input automatically

--- app/Main.hs
+++ app/Main.hs
@@ -1,47 +1,58 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
 module Main where
-  
-import Text.Read   (readMaybe)
-import Data.Maybe  (fromMaybe)
 
-import DayOne.First.DayOneOne   ( mainDayOne  , testDayOne  )
-import DayTwo.First.DayTwoFirst ( mainDayTwo  , testDayTwo  )
-import DayThree.DayThree        ( mainDayThree, testDayThree)
-import DayFour.DayFour          ( mainDayFour , testDayFour )
-import DayFive.DayFive          ( mainDayFive , testDayFive )
-import DaySix.DaySix            ( mainDaySix  , testDaySix  )
-import DaySeven.DaySeven        ( mainDaySeven, testDaySeven)
-import DayEight.DayEight        ( mainDayEight, testDayEight)
-import DayNine.DayNine          ( mainDayNine , testDayNine )
+import           Data.Maybe               (fromMaybe)
+import           Text.Read                (readMaybe)
 
-import qualified Common.Days as Days
+import           DayEight.DayEight        (mainDayEight, testDayEight)
+import           DayFive.DayFive          (mainDayFive, testDayFive)
+import           DayFour.DayFour          (mainDayFour, testDayFour)
+import           DayNine.DayNine          (mainDayNine, testDayNine)
+import           DayOne.First.DayOneOne   (mainDayOne, testDayOne)
+import           DaySeven.DaySeven        (mainDaySeven, testDaySeven)
+import           DaySix.DaySix            (mainDaySix, testDaySix)
+import           DayThree.DayThree        (mainDayThree, testDayThree)
+import           DayTwo.First.DayTwoFirst (mainDayTwo, testDayTwo)
+
+import           Common.Days              (getInput, getPrompt)
+import qualified Common.Days              as Days
+import           System.Exit              (exitFailure)
+import Control.Monad ((>=>))
 
 main :: IO ()
 main = do
-    putStrLn "test/run"
+    putStrLn "test/run/{day}/{day}t/prompt/input/bm/new"
     input <- getLine
-    case input of 
-      "test"    -> testDayOne >> testDayTwo >> testDayThree >> testDayFour >> testDayFive >> testDaySix >> testDaySeven
-      "run"     -> mainDayOne >> mainDayTwo >> mainDayThree >> mainDayFour >> mainDayFive >> testDaySix >> mainDaySeven
+    case input of
+      "test"    -> testDayOne >> testDayTwo >> testDayThree >> testDayFour >> testDayFive >> testDaySix >> testDaySeven >> testDayEight >> testDayNine 
+      "run"     -> mainDayOne >> mainDayTwo >> mainDayThree >> mainDayFour >> mainDayFive >> testDaySix >> mainDaySeven >> mainDayEight >> mainDayNine 
       "1"       -> mainDayOne       -- main 1
-      "1t"      -> testDayOne  
+      "1t"      -> testDayOne
       "2"       -> mainDayTwo       -- main 2
-      "2t"      -> testDayTwo  
+      "2t"      -> testDayTwo
       "3"       -> mainDayThree     -- main 3
-      "3t"      -> testDayThree  
+      "3t"      -> testDayThree
       "4"       -> mainDayFour      -- main 4
-      "4t"      -> testDayFour   
+      "4t"      -> testDayFour
       "5"       -> mainDayFive      -- main 5
-      "5t"      -> testDayFive   
+      "5t"      -> testDayFive
       "6"       -> mainDaySix       -- main 6
-      "6t"      -> testDaySix   
+      "6t"      -> testDaySix
       "7"       -> mainDaySeven     -- main 7
-      "7t"      -> testDaySeven  
+      "7t"      -> testDaySeven
       "8"       -> mainDayEight     -- main 8
-      "8t"      -> testDayEight   
-      "9"       -> mainDayNine     -- main 9
-      "9t"      -> testDayNine   
-      "bm"      -> print   "Please use 'stack bench' for that!"
-      "new"     -> let bl = putStrLn "Are you sure you want to create a new day? (True/False)" *> (fromMaybe False . readMaybe <$> getLine) in 
+      "8t"      -> testDayEight
+      "9"       -> mainDayNine      -- main 9
+      "9t"      -> testDayNine
+      "prompt"  -> let day = print "Please enter a Day Number" *> (fromMaybe 1 . readMaybe <$> getLine) in
+                            day >>= (getPrompt >=> \case
+                                      Left ace  -> print ace *> exitFailure 
+                                      Right _   -> print "The input has been downloaded to cache/prompt/2021/")
+      "input"   -> let day = print "Please enter a Day Number" *> (fromMaybe 1 . readMaybe <$> getLine) in
+                            day >>= (getInput >=> \case
+                                      Left ace  -> print ace *> exitFailure 
+                                      Right _   -> print "The input has been downloaded to inputs/auto/input/2021/")
+      "bm"      -> print "Please use 'stack bench' for that!"
+      "new"     -> let bl = putStrLn "Are you sure you want to create a new day? (True/False)" *> (fromMaybe False . readMaybe <$> getLine) in
                             bl >>= (\x -> if x then Days.initDay else putStrLn "" *> main)
       _         -> error $ "Please enter test or run or num(t). Available Nums are: " ++ show [1..9]
--- src/Common/Days.hs
+++ src/Common/Days.hs
@@ -1,18 +1,26 @@
--- | 
--- | A module that holds 
--- | scripts and functions 
+{-# OPTIONS_GHC -Wno-missing-fields #-}
+-- |
+-- | A module that holds
+-- | scripts and functions
 -- | for setup etc.
 -- |
 ------------------------
 module Common.Days
 ( initDay
+, getPrompt
+, getInput
 ) where
 
-import Data.Maybe (fromMaybe)
-import Text.Read (readMaybe)
+import           Data.Maybe       (fromMaybe)
+import           Data.Text        (Text)
+import           Text.Read        (readMaybe)
 
-import System.Exit (exitFailure)
-import System.Directory (createDirectoryIfMissing, doesFileExist)
+import           Advent           (AoC (AoCInput, AoCPrompt), AoCError,
+                                   AoCOpts (AoCOpts, _aCache, _aForce, _aSessionKey, _aThrottle, _aYear),
+                                   Part, mkDay_, runAoC)
+import qualified Data.Map         as Map
+import           System.Directory (createDirectoryIfMissing, doesFileExist)
+import           System.Exit      (exitFailure)
 
 data DayNumber
     = NoDay
@@ -49,10 +57,41 @@ initDay = do
     doesFileExist ("./src/Day" ++ dayNum' ++ "/" ++ "Day" ++ dayNum' ++ ".hs") >>= \bl ->
         if bl
         then putStrLn "The file already exists, abandoning..." *> exitFailure
-        else 
+        else
             createDirectoryIfMissing True ("./src/Day" ++ dayNum' ++ "/") *>
             writeFile ("./src/Day" ++ dayNum' ++ "/" ++ "Day" ++ dayNum' ++ ".hs") fileTemplate
-        
+
 
 parseDayFile :: Maybe Int -> String -> (Maybe DayNumber, String)
 parseDayFile i str = let ei = (toEnum <$> i :: Maybe DayNumber) in (ei , foldl (\acc x -> if x == 'X' then acc ++ show (fromMaybe NoDay ei) else acc ++ [x]) "" str)
+
+getPrompt :: Integer -> IO (Either Advent.AoCError (Map.Map Advent.Part Text))
+getPrompt day = defaultOpts >>= (\x -> runAoC x $ AoCPrompt (mkDay_ day))
+
+getInput :: Integer -> IO (Either AoCError Text)
+getInput day = inputOpts >>= (\x -> runAoC x $ AoCInput (mkDay_ day))
+
+inputOpts :: IO AoCOpts
+inputOpts = do
+    cookie <- getSessionCookie
+    pure AoCOpts {
+    _aSessionKey = cookie,
+    _aYear = 2021,
+    _aCache = Just "inputs/auto",
+    _aForce = False,
+    _aThrottle = 3000000
+}
+
+defaultOpts :: IO AoCOpts
+defaultOpts = do
+    cookie <- getSessionCookie
+    pure AoCOpts {
+    _aSessionKey = cookie,
+    _aYear = 2021,
+    _aCache = Just "/Users/mycf/Documents/haskell/AOC21STACK/AOC21/cache",
+    _aForce = False,
+    _aThrottle = 3000000
+}
+
+getSessionCookie :: IO String
+getSessionCookie = readFile "config.txt"
