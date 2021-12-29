2188c643c65504c644a32b50db6853db71f69018
0xmycf <mycf.mycf.mycf@gmail.com>
2021-12-29 11:10:10 +0100

    Day11 Part 1 & 2

--- src/DayEleven/DayEleven.hs
+++ src/DayEleven/DayEleven.hs
@@ -3,13 +3,14 @@ module DayEleven.DayEleven
 , testDayEleven
 ) where
 
-import           Common.Lib  (getAllNeighbs, getLines, mapIdx)
-import           Data.Map    (Map)
-import qualified Data.Map    as Map
-import           Data.Maybe  (isJust)
-import           Linear      (V2 (V2))
-import Prelude hiding (all)
-import Data.List ((\\))
+import           Common.Lib (frequencyMap, getAllNeighbs, getLines, mapIdx)
+import           Data.Map   (Map)
+import qualified Data.Map   as Map
+import           Data.Maybe (isJust)
+import qualified Data.Set   as Set
+import           Linear     (V2 (V2))
+import           Prelude    hiding (all)
+import Data.List (unfoldr)
 
 
 inputPath :: FilePath
@@ -18,7 +19,11 @@ inputPath = "./inputs/auto/input/2021/DayEleven.txt"
 testPath :: FilePath
 testPath  = "./inputs/test/DayEleven.txt"
 
+testPath2 :: FilePath
+testPath2  = "./inputs/test/DayEleven2.txt"
+
 type Coordinate = V2 Int
+type Octopi     = Map Coordinate Int
 
 -- | creates a map of key (row, column) (keys are V2) to Int (the octopus)
 input :: FilePath -> IO (Map Coordinate Int)
@@ -34,43 +39,65 @@ testDayEleven :: IO ()
 testDayEleven = do
     putStrLn "Test Day Eleven..."
     inp <- input testPath
-    let foo = step $ inp
-    print $ step $ inp
-    writeFile "./inputs.txt" (show foo)
+    inp2<- input testPath2
+    let foo = fmap setToZero . flash . step $ inp
+        foo2= inp2
+    print foo
+    print $ fmap (==foo2) foo
+    print $ sum . take 100 . unfoldr (Just . fullStep) $ inp
+    print $ length . takeWhile (/=100) . unfoldr (Just . fullStep) $ inp
+    --writeFile "./inputs.txt" (show foo)
     putStrLn "Test Day Eleven over.\n"
 
-
 problemOne :: IO ()
-problemOne = print "to be impl"
+problemOne = do
+    inp <- input inputPath
+    print $ sum . take 100 . unfoldr (Just . fullStep) $ inp
 
 problemTwo :: IO ()
-problemTwo = print "to be impl"
+problemTwo = do
+    inp <- input inputPath
+    print $ 1 + (length . takeWhile (/=100) . unfoldr (Just . fullStep) $ inp )
 
+-- | simulates a full step
+fullStep :: Octopi -> (Int, Octopi)
+fullStep = fmap setToZero . flash . step
 
-step :: Map Coordinate Int -> Map Coordinate Int
--- error his here, because folded map gets not changed obviously!
-step mp = fst .  Map.foldlWithKey (\a@(acc, n) key val -> if val + 1 > 9 then flash a [key] else (Map.insert key (val + 1) acc, n)) (mp, []) $ mp
+-- | adds one level to each octopus
+step :: Octopi -> Octopi
+step = fmap (+1)
+
+-- | takes in the stepped octopi board, then flashes the flashy ones
+-- | and returns a tuple of the amount of flashes and the updated board
+flash :: Octopi -> (Int, Octopi)
+flash octo = (amnt, octo'')
     where
-    flash :: (Map Coordinate Int, [Coordinate]) -> [Coordinate] -> (Map Coordinate Int, [Coordinate])
-    flash all []          = all
-    flash (mp,acc) [c] =
-        let neighbs = (filter justFilter . getAllNeighbs $ c)
-            in go (Map.insert c 0 mp) neighbs (c : acc)
-    flash _ _ = error "never happens"
-    go :: Map Coordinate Int -> [Coordinate] -> [Coordinate] -> (Map Coordinate Int, [Coordinate])
-    go m [] set = (m,set)
-    go map' nbs@(x:xs) set
-        | map' Map.! x + 1 <= 9 = go (Map.insertWith (+) x 1 map') xs set
-        | map' Map.! x + 1 > 9 && x `notElem` set = flash (map', set) [x]
-        --  | map' Map.! x + 1 > 9 && x `notElem` set = go (Map.insert x 0 map') ((filter justFilter . getAllNeighbs $ x) ++ xs) (x : set)
-        | x `elem` set = go map' xs set
-        | otherwise = error $ "map " ++  show map' ++ " nbs " ++ show nbs ++ " set " ++ show set
-    justFilter x = let x' = x `Map.lookup` mp in isJust x'
+    (flashy, sleepy) = Map.partition (>9) octo
+    neighbs = frequencyMap . concatMap (filter (justFilter octo) . getAllNeighbs) . Map.keys $ flashy
+    amnt    = length flashy + amnt2
+    octo'   = Map.unionWith (+) neighbs octo
+    (amnt2 , octo'')  = moreFlashes octo' sleepy
+
+moreFlashes :: Octopi -> Octopi -> (Int, Octopi)
+moreFlashes = go 0
+    where
+    go :: Int -> Octopi -> Octopi -> (Int, Octopi)
+    go i octo sl
+        | null flashy2 = (i, octo)
+        | otherwise     = go (i + length flashy2) octo' sl'
+        where
+        (flashy2, sl')  = Map.partition (>9) $ Map.restrictKeys octo (Set.fromList (Map.keys sl))
+        neighbs2 = frequencyMap . concatMap (filter (justFilter octo) . getAllNeighbs) . Map.keys $ flashy2
+        octo'    = Map.unionWith (+) octo neighbs2
 
 
-        {-
-            What needs to happen inside flash: 
-            Values greater then nine need to be set to 0 unless they have been set to 0 previously, in which case they are inside acc
-            Every other value will be increased by one, unless they will go beyond 9, then the first rule applies
-            If any value hits a number greater than 9, it will also flash
-        -}
+-- | The function that sets all values greater than 9 to zero
+-- | NOTE: Should be applied after all flashes!
+setToZero :: Octopi -> Octopi
+setToZero octo = (0 <$ tooHigh) `Map.union` octo
+    where
+    (tooHigh, _) = Map.partition (>9) octo
+
+-- | filters through allNeighbors to the neighbors which are actually in the map
+justFilter :: Ord k => Map k a -> k -> Bool
+justFilter mp x = let x' = x `Map.lookup` mp in isJust x'
