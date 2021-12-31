84f31a7228573715b18a08fe79945962a0a4dd82
0xmycf <mycf.mycf.mycf@gmail.com>
2021-12-31 03:17:15 +0100

    Day 13 Part 2 -- beware of indices

--- src/DayThirteen/DayThirteen.hs
+++ src/DayThirteen/DayThirteen.hs
@@ -1,4 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 module DayThirteen.DayThirteen
 ( mainDayThirteen
 , testDayThirteen
@@ -6,11 +5,12 @@ module DayThirteen.DayThirteen
 
 import           Common.Lib      (getLines)
 import           Data.Char       (toUpper)
-import           Data.List       (nub, partition)
+import           Data.List       (nub, partition, foldl')
 import           Data.List.Split (splitOn)
-import           Data.Map        (Map)
-import qualified Data.Map        as Map
+import           Data.Matrix     (Matrix)
 import           Linear          (V2 (V2))
+import qualified Data.Map        as Map
+import qualified Data.Matrix     as Matrix
 
 inputPath :: FilePath
 inputPath = "./inputs/auto/input/2021/DayThirteen.txt"
@@ -18,15 +18,12 @@ inputPath = "./inputs/auto/input/2021/DayThirteen.txt"
 testPath :: FilePath
 testPath  = "./inputs/test/DayThirteen.txt"
 
--- | parses the input depending on the FilePath given
-input :: FilePath -> IO (Paper, [Fold])
-input path = let input = partition (\v -> head v == 'f') . filter (/=[]) <$> getLines path
-             in do
-                (folds, nums) <- input
-                pure (toMap nums, toFold folds)
+-- | slightly changed input to make part 2 easier
+input' :: FilePath -> IO ([Point], [Fold])
+input' path = let input = partition (\v -> head v == 'f') . filter (/=[]) <$> getLines path
+             in input >>= \(folds, nums) -> pure (fmap ((\(x:y:_) -> V2 (read x) (read y)) . splitOn ",") nums, toFold folds)
 
 type Point = V2 Int
-type Paper = Map Point Bool
 
 data Axis = X | Y deriving (Read, Show, Eq)
 data Fold = Fold {
@@ -40,28 +37,34 @@ mainDayThirteen = putStrLn "Day Thirteen..." >> problemOne >> problemTwo >> putS
 testDayThirteen :: IO ()
 testDayThirteen = do
     putStrLn "Test Day Thirteen..."
-    inp <- input testPath
-    print inp
+    (points, _) <- input' testPath
+    print points
+    writeFile "inputs/test/parsedout/13.txt" (show . draw $ points)
     putStrLn "Test Day Thirteen over.\n"
 
 
 problemOne :: IO ()
-problemOne = do
-    (paper, f:_) <- input inputPath
-    let (yesFold, noFold) = Map.partitionWithKey (part f) paper
-        yesFold'          = fmap fst . Map.toList $ yesFold -- using a map here was not the right choice haha
-        noFold'           = fmap fst . Map.toList $ noFold
-    print $ length $ nub $ noFold' ++ fmap (mirror f) yesFold'
-    where
-    part :: Fold -> Point -> Bool -> Bool
-    part f (V2 x y) _ = case _axis f of
-        X -> x > _val f
-        Y -> y > _val f
-
+problemOne = input' inputPath >>= \(points, f:_) -> let (yesFold, noFold) = partition (part f) points
+                                                    in print . length . nub $ noFold ++ fmap (mirror f) yesFold
 
 problemTwo :: IO ()
-problemTwo = print "to be impl"
+problemTwo = input' inputPath >>= \(points, fs) -> writeFile "inputs/test/parsedout/13.txt" (show . draw . foldAll points $ fs)
+    where
+    foldAll :: [Point] -> [Fold] -> [Point]
+    foldAll = foldl' \acc v ->
+        let (yesFold, noFold) = partition (part v) acc
+        in noFold ++ fmap (mirror v) yesFold
 
+    -- For memories without foldl':
+    -- foldAll ps []     = nub ps
+    -- foldAll ps (f:fs) =
+    --     let (yesFold, noFold) = partition (part f) ps in
+    --         foldAll (noFold ++ fmap (mirror f) yesFold) fs
+
+part :: Fold -> Point -> Bool
+part f (V2 x y) = case _axis f of
+    X -> x > _val f
+    Y -> y > _val f
 {-
     Parsing
     Through string manipulation
@@ -69,17 +72,13 @@ problemTwo = print "to be impl"
     Works only on valid input for this problem.
 -}
 
-toMap :: [String] -> Paper
-toMap xs = Map.fromList (xs' `zip` repeat True)
-    where
-    xs' = fmap ((\(x:y:_) -> V2 (read x) (read y)) . splitOn ",") xs
-
 toFold :: [String] -> [Fold]
 toFold = fmap folding
     where
     folding :: String -> Fold
     folding ('f':'o':'l':'d':' ':'a':'l':'o':'n':'g':' ':a:'=':xs) = Fold {_axis = read [toUpper a] , _val = read xs}
     folding i = error $ "Invalid input: " ++ show i
+
 
 {-
     Some basic geometry:
@@ -103,13 +102,22 @@ toFold = fmap folding
     and the distance is just the absolute value of difference of the other components.
 -}
 
--- | distance for integer grid points on one line
+-- | Distance for integer grid points on one line
 dist :: Num a => V2 a -> V2 a -> a
 dist (V2 a b) (V2 x y) = abs $ (a - x) + (b - y)
 
--- | mirrors a Point p on the line g
---   this function is constrained to the problem (g lies either on the X or Y axis)
--- mirror :: Fold -> Point -> Point
+-- | mirrors a Point p on the line g.
+--   This function is constrained to the problem (g lies either on the X or Y axis)
 mirror :: Fold -> Point -> Point
 mirror (Fold Y v) p@(V2 x _) = V2 x (v - dist p (V2 x v))
 mirror (Fold X v) p@(V2 _ y) = V2 (v - dist p (V2 v y)) y
+
+
+-- | draws the grid
+--   Abusing the Matrix package to draw my things :)
+--   Btw this function cost me 2 hours because I didn't realize that I need a Matrix with 51 51 instead of 50 50...
+draw :: [Point] -> Matrix Int
+draw ps = let mp  = Map.fromList ([V2 x y | x <- [0..50], y <- [0..50]] `zip` repeat False)
+              mp' = Map.union (Map.fromList (ps `zip` repeat True)) mp
+          in
+          Matrix.transpose $ Matrix.fromList 51 51 ((\v -> if v then 1 else 0) <$> Map.elems mp')
