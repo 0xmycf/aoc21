00a29bab24ec2c77fadbd38100d213b5904524f0
0xmycf <mycf.mycf.mycf@gmail.com>
2022-01-05 00:14:58 +0100

    Day 16 Parsing

--- src/DaySixteen/DaySixteen.hs
+++ src/DaySixteen/DaySixteen.hs
@@ -3,8 +3,11 @@ module DaySixteen.DaySixteen
 , testDaySixteen
 ) where
 
-import           Common.Lib  (getLines, parse)
-import qualified Text.Parsec as Parsec
+import           Common.Lib (getLines)
+import           Data.Map   (Map, lookup)
+import qualified Data.Map   as Map
+import           Data.Maybe (mapMaybe)
+import           Prelude    hiding (lookup)
 
 inputPath :: FilePath
 inputPath = "./inputs/auto/input/2021/DaySixteen.txt"
@@ -15,10 +18,19 @@ testPath  = "./inputs/test/DaySixteen.txt"
 outPath :: FilePath
 outPath = "./inputs/test/parsedout/DaySixteen.txt"
 
+mapping :: IO (Map Char String)
+mapping = do
+    inp <- getLines "inputs/DaySixteenMapping.txt"
+    pure $ Map.fromList $
+        fmap toTuple inp
+    where
+    toTuple :: String -> (Char, String)
+    toTuple (a:xs) = (a, xs)
+    toTuple _      = ('Z', "")
+
 -- | parses the input depending on the FilePath given
-input :: FilePath -> IO ()
-input path = let inputs = id <$> getLines path
-             in pure ()
+input :: FilePath -> IO String
+input path = mapping >>= \x -> concat . mapMaybe (`lookup` x) <$> readFile path
 
 mainDaySixteen :: IO ()
 mainDaySixteen = putStrLn "Day Sixteen..." >> problemOne >> problemTwo >> putStrLn "Day Sixteen over.\n "
