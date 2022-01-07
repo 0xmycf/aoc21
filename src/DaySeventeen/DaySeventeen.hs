2e747e0944536a0bdfecb99fb4662cb04c4322db
0xmycf <mycf.mycf.mycf@gmail.com>
2022-01-07 18:13:49 +0100

    Changed DayX.tmp to make benching neater

--- src/DaySeventeen/DaySeventeen.hs
+++ src/DaySeventeen/DaySeventeen.hs
@@ -3,8 +3,9 @@ module DaySeventeen.DaySeventeen
 , testDaySeventeen
 ) where
 
-import           Common.Lib  (getLines, parse)
-import qualified Text.Parsec as Parsec
+import           Common.Lib    (getLines, parse)
+import           Control.Arrow ((&&&))
+import qualified Text.Parsec   as P
 
 inputPath :: FilePath
 inputPath = "./inputs/auto/input/2021/DaySeventeen.txt"
@@ -15,22 +16,21 @@ testPath  = "./inputs/test/DaySeventeen.txt"
 outPath :: FilePath
 outPath = "./inputs/test/parsedout/DaySeventeen.txt"
 
+type Input = [String]
+
 -- | parses the input depending on the FilePath given
-input :: FilePath -> IO ()
+input :: FilePath -> IO Input
 input path = let inputs = id <$> getLines path
-             in pure ()
+             in inputs
 
 mainDaySeventeen :: IO ()
-mainDaySeventeen = putStrLn "Day Seventeen..." >> problemOne >> problemTwo >> putStrLn "Day Seventeen over.\n "
+mainDaySeventeen = putStrLn "Day Seventeen..." >> input inputPath >>= print . (problemOne &&& problemTwo) >> putStrLn "Day Seventeen over.\n "
 
 testDaySeventeen :: IO ()
-testDaySeventeen = do
-    putStrLn "Test Day Seventeen..."
-    putStrLn "Test Day Seventeen over.\n"
+testDaySeventeen = putStrLn "Day Seventeen..." >> input testPath >>= print . (problemOne &&& problemTwo) >> putStrLn "Day Seventeen over.\n "
 
+problemOne :: Input -> String
+problemOne = const "to be impl"
 
-problemOne :: IO ()
-problemOne = print "to be impl"
-
-problemTwo :: IO ()
-problemTwo = print "to be impl"
+problemTwo :: Input -> String
+problemTwo = const "to be impl"
